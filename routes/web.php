343f167a995ab60234aaccd43931e01b26d880b9
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-03-01 08:12:37 +0700

    Add user view and route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,9 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    return view("welcome");
 });
 
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get("/level", [LevelController::class, "index"]);
+Route::get("/kategori", [KategoriController::class, "index"]);
+Route::get("/user", [UserController::class, "index"]);
