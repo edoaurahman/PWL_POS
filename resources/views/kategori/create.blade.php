d9c09a0aec31e49ef4d6a7e3cc4d149e5c08dfc8
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-03-15 09:33:48 +0700

    Add laravel-datatables-vite and bootstrap dependencies, update VerifyCsrfToken middleware, fix relationship in KategoriModel, and use Vite in AppServiceProvider

---
+++ resources/views/kategori/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layout.app')
+@section('subtitle', 'Katagori')
+@section('content_header_title','Katagori')
+@section('content_header_subtitle','Create')
+
+@section('content')
+<div class="cotainer">
+    <div class="card card-primary">
+        <div class="card-header">
+            <h3 class="card-title">Buat kategori baru</h3>
+        </div>
+
+        <form action="{{ url('/kategori') }}" method="post">
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="kodeKategori">Kode Kategori</label>
+                    <input type="text" class="form-control" id="kodeKategori" name="kodeKategori"
+                        placeholder="Untuk makanan cnt: MKN">
+                </div>
+                <div class="form-group">
+                    <label for="namaKategori">Nama Kategori</label>
+                    <input type="text" class="form-control" id="namaKategori" name="namaKategori"
+                        placeholder="Nama Kategori">
+                </div>
+
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
